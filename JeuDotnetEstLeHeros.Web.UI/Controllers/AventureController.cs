4fbc4a7c15bf69fbe446df564e8433a56059c006
Willian Oliveira Siqueira Cavalcante <woscavalcante@outlook.com>
2021-02-18 07:39:23 -0300

    Adicionar arquivos de projeto.

---
+++ JeuDotnetEstLeHeros.Web.UI/Controllers/AventureController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JeuDotnetEstLeHeros.Web.UI.Controllers
+{
+    public class AventureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
