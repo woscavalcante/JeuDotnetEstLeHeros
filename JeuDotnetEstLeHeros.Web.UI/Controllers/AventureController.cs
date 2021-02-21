c08313235d969e7a5ad20366858bc224d597c3dc
Willian Oliveira Siqueira Cavalcante <woscavalcante@outlook.com>
2021-02-21 16:49:41 -0300

    Le Models et dlls

--- JeuDotnetEstLeHeros.Web.UI/Controllers/AventureController.cs
+++ JeuDotnetEstLeHeros.Web.UI/Controllers/AventureController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JeuDotnetEstLeHeros.Web.UI.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using jeudontonestleheros.Core.Data;
 
 namespace JeuDotnetEstLeHeros.Web.UI.Controllers
 {
@@ -11,9 +13,22 @@ namespace JeuDotnetEstLeHeros.Web.UI.Controllers
         public IActionResult Index()
         {
             this.ViewBag.MonTitre = "Aventures";
-            this.ViewBag.MonTableau = new int[] { 1, 2, 3, 4, 5 };
 
-            return View();
+            List<Aventure> maList = new List<Aventure>();
+
+            maList.Add(new Aventure()
+            {
+                Id = 1,
+                Titre = "Ma première aventure"
+            });
+
+            maList.Add(new Aventure()
+            {
+                Id = 2,
+                Titre = "Ma seconde aventure"
+            });
+
+            return View(maList);
         }
     }
 }
